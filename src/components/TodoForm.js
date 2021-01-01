349fa73d949fa97c2e9f3b599e1678e59b995399
JoshHaokip <joshhaokip@gmail.com>
2021-01-01 19:03:11 +0000

    Remove last commit changes - TEST COMPLETE

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -38,7 +38,7 @@ function TodoForm(props) {
         borderStyle: 'solid',
         borderWidth: 'thin',
         borderColor: '#6EE7B7',
-        backgroundColor: 'red',
+        backgroundColor: '#6EE7B7',
         color: 'white',
         letterSpacing: '2px',
         fontSize: '15px',
